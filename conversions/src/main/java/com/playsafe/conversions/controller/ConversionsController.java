944d1cc52725e5dcb068c42590be63ccafd31faf
sbondai <sbondai9@gmail.com>
2021-01-15 09:46:53 +0200

    Created Service Layer and Kevin Util for Conversions

--- conversions/src/main/java/com/playsafe/conversions/controller/ConversionsController.java
+++ conversions/src/main/java/com/playsafe/conversions/controller/ConversionsController.java
@@ -1,10 +1,26 @@
 package com.playsafe.conversions.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.playsafe.conversions.service.ConversionService;
+import com.playsafe.conversions.utils.Kelvin;
 
 @RestController
 @RequestMapping("/conversions")
 public class ConversionsController {
+
+	@Autowired
+	ConversionService conversionService;
+
+	@PostMapping("/ktoc")
+	double convertKelvintoCelcius(@Valid @RequestBody Kelvin kelvin) {
+		return conversionService.convertKelvintoDegrees(kelvin.getEnteredValue());
+	}
 
 }
