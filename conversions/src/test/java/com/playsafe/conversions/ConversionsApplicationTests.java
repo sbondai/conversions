b9ed4fb3280fabc0b137844e41013cc8297a4866
sbondai <sbondai9@gmail.com>
2021-01-15 09:24:34 +0200

    Created new project in STS using the Spring Starter bundle

---
+++ conversions/src/test/java/com/playsafe/conversions/ConversionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playsafe.conversions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConversionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
